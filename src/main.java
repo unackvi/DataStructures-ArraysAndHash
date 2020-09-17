51db2c41055119bcdffc643677c2145f94ce7ce6
UzmaS <unackvi@gmail.com>
2020-09-17 12:21:04 -0500

    first checkin

---
+++ src/main.java
@@ -0,0 +1,31 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LinkedList<String> linky = new LinkedList<String>();
+		linky.add("Rob");
+		linky.add("Alex");
+		linky.add("Jose");
+		linky.add("John");
+		
+		System.out.println(linky.getFirst());
+		
+		Iterator it = linky.iterator();
+		while (it.hasNext()) {
+			if((String) it.next() == "Alex") {
+				System.out.println("Alex is found");
+				break;
+			} else {
+				System.out.println("Alex is not a member!");
+			}
+		}
+		
+		
+
+	}
+
+}
